ae1c44d6294dc8f885669fd68771cd839cbb0ab6
dstore-dbap <b.puttmann@dbap.de>
2015-04-29 14:33:19 +0200

    Updated test for DnsLookup module.

--- tests/TestAddDnsLookup.py
+++ tests/TestAddDnsLookup.py
@@ -1,3 +1,4 @@
+import time
 import extendSysPath
 import ModuleBaseTestCase
 import mock
@@ -14,8 +15,12 @@ class TestAddDnsLookup(ModuleBaseTestCase.ModuleBaseTestCase):
         config = {'source_fields': 'host'}
         self.test_object.configure(config)
         self.checkConfiguration()
+        self.test_object.initAfterFork()
         data = Utils.getDefaultEventDict({'host': 'www.dbap.de'})
-        for event in self.test_object.handleEvent(data):
+        self.test_object.receiveEvent(data)
+        # Wait for interval.
+        time.sleep(1)
+        for event in self.receiver.getEvent():
             self.assertTrue(event['host'] == '195.137.224.39')
 
     def testDnsLookupWithTargetField(self):
@@ -23,17 +28,25 @@ class TestAddDnsLookup(ModuleBaseTestCase.ModuleBaseTestCase):
                   'target_field': 'host_ip_address'}
         self.test_object.configure(config)
         self.checkConfiguration()
+        self.test_object.initAfterFork()
         data = Utils.getDefaultEventDict({'host': 'www.dbap.de'})
-        for event in self.test_object.handleEvent(data):
+        self.test_object.receiveEvent(data)
+        # Wait for interval.
+        time.sleep(1)
+        for event in self.receiver.getEvent():
             self.assertTrue(event['host_ip_address'] == '195.137.224.39')
 
     def testDnsLookupWithMultipleSourceFields(self):
         config = {'source_fields': ['host1', 'host2', 'host3']}
         self.test_object.configure(config)
         self.checkConfiguration()
+        self.test_object.initAfterFork()
         data = Utils.getDefaultEventDict({'host1': 'www.aksjdhakjsgdag.de',
                                           'host3': 'www.dbap.de'})
-        for event in self.test_object.handleEvent(data):
+        self.test_object.receiveEvent(data)
+        # Wait for interval.
+        time.sleep(1)
+        for event in self.receiver.getEvent():
             self.assertTrue(event['host3'] == '195.137.224.39')
 
     def testDnsLookupWithMultipleSourceFieldsAndTargetField(self):
@@ -41,9 +54,13 @@ class TestAddDnsLookup(ModuleBaseTestCase.ModuleBaseTestCase):
                   'target_field': 'host_ip_address'}
         self.test_object.configure(config)
         self.checkConfiguration()
+        self.test_object.initAfterFork()
         data = Utils.getDefaultEventDict({'host1': 'www.aksjdhakjsgdag.de',
                                           'host3': 'www.dbap.de'})
-        for event in self.test_object.handleEvent(data):
+        self.test_object.receiveEvent(data)
+        # Wait for interval.
+        time.sleep(1)
+        for event in self.receiver.getEvent():
             self.assertTrue(event['host_ip_address'] == '195.137.224.39')
 
     def testReverseDnsLookup(self):
@@ -51,8 +68,12 @@ class TestAddDnsLookup(ModuleBaseTestCase.ModuleBaseTestCase):
                   'source_fields': 'remote_ip'}
         self.test_object.configure(config)
         self.checkConfiguration()
+        self.test_object.initAfterFork()
         data = Utils.getDefaultEventDict({'remote_ip': '127.0.0.1'})
-        for event in self.test_object.handleEvent(data):
+        self.test_object.receiveEvent(data)
+        # Wait for interval.
+        time.sleep(1)
+        for event in self.receiver.getEvent():
             result = event['remote_ip'].split('.')[0]
             self.assertTrue(result == 'localhost')
 
