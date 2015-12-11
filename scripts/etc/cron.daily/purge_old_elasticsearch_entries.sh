d6f74a213715df71a2b1c6a7d418d9e93d0c120a
dstore-dbap <b.puttmann@dbap.de>
2015-12-11 14:09:46 +0100

    Testcommit

--- scripts/etc/cron.daily/purge_old_elasticsearch_entries.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Remove entries from elasticsearch index older than --day-to-keep days.
-
-/opt/LumberMill/src/scripts/logstash_index_cleaner.py --host es-01.dbap.de --port 9200 --timeout 60 --prefix lumbermill- --days-to-keep 10
-/opt/LumberMill/src/scripts/logstash_index_cleaner.py --host es-01.dbap.de --port 9200 --timeout 60 --prefix logstash- --days-to-keep 10
-/opt/LumberMill/src/scripts/logstash_index_cleaner.py --host es-01.dbap.de --port 9200 --timeout 60 --prefix agora_access- --days-to-keep 10
