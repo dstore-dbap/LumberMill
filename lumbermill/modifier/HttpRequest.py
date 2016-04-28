91f190b8469347e54bc1b0042c192d6f5bb6abb5
dstore-dbap <b.puttmann@dbap.de>
2016-04-28 13:05:58 +0200

    Added option to also get metadata of request.

--- lumbermill/modifier/HttpRequest.py
+++ lumbermill/modifier/HttpRequest.py
@@ -3,25 +3,40 @@ import socket
 import sys
 import urllib2
 
+import lumbermill.utils.DictUtils as DictUtils
 from lumbermill.BaseThreadedModule import BaseThreadedModule
-from lumbermill.utils.Decorators import ModuleDocstringParser
-
+from lumbermill.utils.Decorators import ModuleDocstringParser, setInterval
+from lumbermill.utils.misc import TimedFunctionManager
 
 @ModuleDocstringParser
 class HttpRequest(BaseThreadedModule):
     """
     Issue an arbitrary http request and store the response in a configured field.
 
+    If the <interval> value is set, this module will execute the configured request
+    every <interval> seconds and emits the result in a new event.
+
     This module supports the storage of the responses in an redis db. If redis_store is set,
     it will first try to retrieve the response from redis via the key setting.
     If that fails, it will execute the http request and store the result in redis.
+
+    url: The url to grab. Can also contain templated values for dynamic replacement with event data.
+    socket_timeout: The socket timeout in seconds after which a request is considered failed.
+    get_metadata: Also get metadata like headers, encoding etc.
+    target_field: Specifies the name of the field to store the retrieved data in.
+    interval: Number of seconds to wait before calling <url> again.
+    redis_store: Redis address to cache crawling results.
+    redis_key: The key to use for storage in redis.
+    redis_ttl: TTL for data in redis.
 
     Configuration template:
 
     - HttpRequest:
        url:                             # <type: string; is: required>
        socket_timeout:                  # <default: 25; type: integer; is: optional>
+       get_metadata:                    # <default: False; type: boolean; is: optional>
        target_field:                    # <default: "gambolputty_http_request"; type: string; is: optional>
+       interval:                        # <default: None; type: None||float||integer; is: optional>
        redis_store:                     # <default: None; type: None||string; is: optional>
        redis_key:                       # <default: None; type: None||string; is: optional if redis_store is None else required>
        redis_ttl:                       # <default: 60; type: integer; is: optional>
@@ -34,6 +49,8 @@ class HttpRequest(BaseThreadedModule):
     def configure(self, configuration):
         BaseThreadedModule.configure(self, configuration)
         socket.setdefaulttimeout(self.getConfigurationValue('socket_timeout'))
+        self.get_metadata = self.getConfigurationValue('get_metadata')
+        self.interval = self.getConfigurationValue('interval')
         # Get redis client module.
         if self.getConfigurationValue('redis_store'):
             mod_info = self.lumbermill.getModuleInfoById(self.getConfigurationValue('redis_store'))
@@ -41,25 +58,46 @@ class HttpRequest(BaseThreadedModule):
         else:
             self.redis_store = None
 
+    def getRunTimedFunctionsFunc(self):
+        @setInterval(self.interval, call_on_init=True)
+        def runTimedFunctionsFunc():
+            event = DictUtils.getDefaultEventDict({"data": ''}, caller_class_name="HttpRequest")
+            self.receiveEvent(event)
+        return runTimedFunctionsFunc
+
+    def initAfterFork(self):
+        if self.interval:
+            TimedFunctionManager.startTimedFunction(self.getRunTimedFunctionsFunc())
+        BaseThreadedModule.initAfterFork(self)
+
     def handleEvent(self, event):
         request_url = self.getConfigurationValue('url', event)
         target_field_name = self.getConfigurationValue('target_field', event)
         if not request_url or not target_field_name:
             yield event
             return
-        result = None
+        response = None
         if self.redis_store:
             redis_key = self.getConfigurationValue('redis_key', event)
-            result = self.redis_store.get(redis_key)
-        if result is None:
+            response = self.redis_store.get(redis_key)
+        if response is None:
             try:
-                result = self.execRequest(request_url).read()
-                if result and self.redis_store:
-                    self.redis_store.set(redis_key, result, self.getConfigurationValue('redis_ttl'))
+                response = self.execRequest(request_url)
+                if response and self.redis_store:
+                    self.redis_store.set(redis_key, response, self.getConfigurationValue('redis_ttl'))
             except:
                 yield event
                 return
-        event[target_field_name] =  result
+        event[target_field_name] = {'content': response.read(),
+                                    'status_code': response.getcode(),
+                                    'url': response.geturl()}
+        if self.get_metadata:
+            event[target_field_name]['metadata'] = {'headers': response.info().headers,
+                                                    'parameter_list': response.info().getplist(),
+                                                    'encoding': response.info().getencoding(),
+                                                    'type': response.info().gettype(),
+                                                    'maintype': response.info().getmaintype(),
+                                                    'subtype': response.info().getsubtype()}
         yield event
 
     def execRequest(self, url):
