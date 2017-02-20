92612e76365ec697dc75992f5c3a05133a9bae10
dstore-dbap <b.puttmann@dbap.de>
2017-02-20 13:52:46 +0100

    Added more tests for geo info module.

--- tests/TestAddGeoInfo.py
+++ tests/TestAddGeoInfo.py
@@ -41,5 +41,33 @@ class TestAddGeoInfo(ModuleBaseTestCase.ModuleBaseTestCase):
         for event in self.test_object.handleEvent(dict):
             self.assertEqual(event['geo_info']['country_code'], 'US')
 
+    def testAddGeoInfoFromListField(self):
+        self.test_object.configure({'geo_info_fields': ['country_code'],
+                                    'source_fields': ['x_forwarded_for']})
+        self.checkConfiguration()
+        dict = DictUtils.getDefaultEventDict({'x_forwarded_for': ['99.124.167.129']})
+        for event in self.test_object.handleEvent(dict):
+            self.assertEqual(event['geo_info']['country_code'], 'US')
+
+    def testIsValidIpV4Address(self):
+        self.test_object.configure({'geo_info_fields': ['country_code']})
+        self.checkConfiguration()
+        self.assertTrue(self.test_object.is_valid_ipv4_address('127.0.0.1'))
+
+    def testIsInvalidIpV4Address(self):
+        self.test_object.configure({'geo_info_fields': ['country_code']})
+        self.checkConfiguration()
+        self.assertFalse(self.test_object.is_valid_ipv4_address('1234567890'))
+
+    def testIsValidIpV6Address(self):
+        self.test_object.configure({'geo_info_fields': ['country_code']})
+        self.checkConfiguration()
+        self.assertTrue(self.test_object.is_valid_ipv6_address('::1'))
+
+    def testIsInvalidIpV6Address(self):
+        self.test_object.configure({'geo_info_fields': ['country_code']})
+        self.checkConfiguration()
+        self.assertFalse(self.test_object.is_valid_ipv6_address('1234567890'))
+
 if __name__ == '__main__':
     unittest.main()
